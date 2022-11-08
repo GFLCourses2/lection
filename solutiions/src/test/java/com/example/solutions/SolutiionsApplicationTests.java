4b75406a904644d4b60bf9213237b9622a11ee4e
OlehNoskov <olega6000@gmail.com>
2022-11-08 21:52:17 +0200

    create new module solutions

---
+++ solutiions/src/test/java/com/example/solutions/SolutiionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.solutions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolutiionsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
