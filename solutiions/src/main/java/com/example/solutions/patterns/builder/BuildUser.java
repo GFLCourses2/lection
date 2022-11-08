98dbf7dec8f0c88673f7b044bdcc313659a10515
OlehNoskov <olega6000@gmail.com>
2022-11-08 22:31:21 +0200

    Creare new class BuildUser

---
+++ solutiions/src/main/java/com/example/solutions/patterns/builder/BuildUser.java
@@ -0,0 +1,47 @@
+package com.example.solutions.patterns.builder;
+
+public class BuildUser {
+
+    private String login;
+    private String password;
+    private Long timestamp;
+
+    public BuildUser(String login, String password, Long timestamp) {
+        this.login = login;
+        this.password = password;
+        this.timestamp = timestamp;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Long getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Long timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    @Override
+    public String toString() {
+        return "BuildUser{" +
+                "login='" + login + '\'' +
+                ", password='" + password + '\'' +
+                ", timestamp=" + timestamp +
+                '}';
+    }
+}
